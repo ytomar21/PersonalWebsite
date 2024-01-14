17419b731c90ae3828f373ff57ca04fdfdda29c1
Yajur21 <yajur.tomar@gmail.com>
2024-01-14 15:57:04 -0500

    Blue box and gray triangle

--- personal-website/app/page.tsx
+++ personal-website/app/page.tsx
@@ -1,13 +1,16 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <main className="Main Page">
+    <main className="flex min-h-screen flex-col p-6">
 
-      <div className="Title">
-        <h2>My name is Yajur Tomar, welcome to my website!</h2>
-      </div>
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        <h2 className="text-white-500">My name is Yajur Tomar, welcome to my website!</h2>
+      </div>        
 
+      <div
+  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-red border-l-transparent border-r-transparent"
+/>
 
     </main>
   )
