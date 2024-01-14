e6336fb8a5627b7d890e57cf8e1bf04d5331bdc3
Yajur21 <yajur.tomar@gmail.com>
2024-01-14 15:40:10 -0500

    Hello world, my website

---
+++ personal-website/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="Main Page">
+
+      <div className="Title">
+        <h2>My name is Yajur Tomar, welcome to my website!</h2>
+      </div>
+
+
+    </main>
+  )
+}
