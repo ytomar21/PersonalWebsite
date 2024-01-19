9643393bfa314f3ee82e3e29d08869ebbe1109de
Yajur21 <yajur.tomar@gmail.com>
2024-01-19 18:57:46 -0500

    Added profile pic

--- personal-website/app/page.tsx
+++ personal-website/app/page.tsx
@@ -1,16 +1,41 @@
-
+import Image from 'next/image'
+import profilePic from './ProfilePic.png'
 
 export default function Home() {
   return (
+    
     <main className="flex min-h-screen flex-col p-6">
 
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <h2 className="text-white-500">My name is Yajur Tomar, welcome to my website!</h2>
-      </div>        
 
-      <div
-  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-red border-l-transparent border-r-transparent"
-/>
+      <div className="grid grid-cols-3 content-center rounded-lg bg-blue-500">
+
+        <div>
+          <Image
+            src={profilePic}
+            width={150}
+            height={150}
+            alt="Profile Pic"
+            className="border-solid border-2 rounded-lg"
+          />
+        </div>
+
+        <div className="col-span-2 flex items-center">
+          <span className="text-white-500 text-xl font-mono">My name is Yajur Tomar, welcome to my website!</span>
+        </div>
+        
+      </div>
+
+      <div className="grid grid-cols-3 content-center rounded-lg bg-blue-500">
+        <span>About Me:</span>
+      </div>
+
+      <div className="grid grid-cols-3 content-center rounded-lg bg-blue-500">
+        <span>Resume</span>
+      </div>
+
+      <div className="grid grid-cols-3 content-center rounded-lg bg-blue-500">
+        <span>Projects</span>
+      </div>     
 
     </main>
   )
